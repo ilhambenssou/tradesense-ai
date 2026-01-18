deb76663373b418391cc4ebf9691ab8922133f8d
ilhambenssou <ilhambenssou@gmail.com>
2026-01-18 03:14:05 +0100

    Update import statements to remove backend prefix

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,14 @@
 
 from flask import Flask, request, make_response
 from threading import Lock
-from backend.config import Config, ProductionConfig
-from backend.error_handlers import register_error_handlers
-from backend.trade_routes import trade_bp
-from backend.admin_routes import admin_bp
-from backend.leaderboard_routes import leaderboard_bp
-from backend.challenge_routes import challenge_routes_bp
-from backend.news_routes import news_bp
-from backend.ai_routes import ai_bp
+from config import Config, ProductionConfig
+from error_handlers import register_error_handlers
+from trade_routes import trade_bp
+from admin_routes import admin_bp
+from leaderboard_routes import leaderboard_bp
+from challenge_routes import challenge_routes_bp
+from news_routes import news_bp
+from ai_routes import ai_bp
 
 challenges_db = {}
 challenge_locks = {}
